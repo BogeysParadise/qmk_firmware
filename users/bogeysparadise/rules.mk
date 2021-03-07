509c0f56fb41303a273fdd5f2ff2db95064059b9
BogeysParadise <60124578+BogeysParadise@users.noreply.github.com>
2021-03-07 16:26:58 -0600

    Add digital rain.

--- users/bogeysparadise/rules.mk
+++ users/bogeysparadise/rules.mk
@@ -7,12 +7,16 @@ ifneq ($(PLATFORM),CHIBIOS)
 endif
 
 GRAVE_ESC_ENABLE      = yes
-TAP_DANCE_ENABLE 	  = no       # Need to add code to enable.
+#TAP_DANCE_ENABLE 	  = no       # Need to add code to enable.
 COMBO_ENABLE 		  = yes      # Enable Combos
 
 #ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
 #    SRC += tap_dances.c
 #endif
+
+ifeq ($(strip $(TAP_DANCE_ENABLE)), yes)
+    SRC += tap_dances.c
+endif
 
 ifeq ($(strip $(RGBLIGHT_ENABLE)), yes)
     SRC += rgb_stuff.c
@@ -45,4 +49,27 @@ endif
 
 ifeq ($(strip $(OLED_DRIVER_ENABLE)), yes)
     SRC += oled_stuff.c
+endif
+
+ifeq ($(strip $(SPLIT_KEYBOARD)), yes)
+    ifneq ($(strip $(SPLIT_TRANSPORT)), custom)
+        SPLIT_TRANSPORT = custom
+        QUANTUM_LIB_SRC += drashna_transport.c
+        OPT_DEFS += -DDRASHNA_CUSTOM_TRANSPORT
+        # Unused functions are pruned away, which is why we can add multiple drivers here without bloat.
+        ifeq ($(PLATFORM),AVR)
+            ifneq ($(NO_I2C),yes)
+                QUANTUM_LIB_SRC += i2c_master.c \
+                                   i2c_slave.c
+            endif
+        endif
+
+        SERIAL_DRIVER ?= bitbang
+        OPT_DEFS += -DSERIAL_DRIVER_$(strip $(shell echo $(SERIAL_DRIVER) | tr '[:lower:]' '[:upper:]'))
+        ifeq ($(strip $(SERIAL_DRIVER)), bitbang)
+            QUANTUM_LIB_SRC += serial.c
+        else
+            QUANTUM_LIB_SRC += serial_$(strip $(SERIAL_DRIVER)).c
+        endif
+    endif
 endif
